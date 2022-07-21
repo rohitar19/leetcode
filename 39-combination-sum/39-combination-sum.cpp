74c1f68368c1c92317fd71917a03b19f8a1ed440
rohitar19 <91067722+rohitar19@users.noreply.github.com>
2022-07-21 13:42:24 +0530

    Time: 3 ms (98.00%), Space: 11 MB (87.20%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.cpp
@@ -0,0 +1,34 @@
+class Solution {
+    public:
+     void printCombination(int ind ,vector<int>&arr ,int target ,vector<vector<int>>&ans , vector<int>&ds ){
+         if(ind==arr.size()){
+             if(target==0)
+                 ans.push_back(ds);
+             
+             return;
+             
+         }
+         
+         if(arr[ind]<=target){
+             //pick the element means ind not change 
+             ds.push_back(arr[ind]);
+             printCombination(ind, arr, target-arr[ind] , ans , ds);
+             ds.pop_back();
+             
+         }
+         //not picking the elemnt
+         printCombination(ind+1, arr, target , ans , ds);
+         
+         
+         
+     }
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>>ans;
+        vector<int>ds;
+        printCombination(0,candidates,target,ans , ds);
+        return ans;
+        
+        
+    }
+};
