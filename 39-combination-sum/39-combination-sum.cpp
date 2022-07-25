a439d01db7f1f90675108aa05b6e6ed7a84f2a3d
rohitar19 <91067722+rohitar19@users.noreply.github.com>
2022-07-25 13:33:01 +0530

    Time: 8 ms (80.94%), Space: 11.1 MB (68.79%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,32 +1,33 @@
 class Solution {
-    public:
-     void printCombination(int ind ,vector<int>&arr ,int target ,vector<vector<int>>&ans , vector<int>&ds ){
-         if(ind==arr.size()){
-             if(target==0)
-                 ans.push_back(ds);
-             
-             return;
-             
-         }
-         
-         if(arr[ind]<=target){
-             //pick the element means ind not change 
-             ds.push_back(arr[ind]);
-             printCombination(ind, arr, target-arr[ind] , ans , ds);
-             ds.pop_back();
-             
-         }
-         //not picking the elemnt
-         printCombination(ind+1, arr, target , ans , ds);
-         
-         
-         
-     }
+  public:
+    void  printCombination( int ind , int target , vector<int>&arr , vector<int>&ds , vector<vector<int>>&ans){
+        
+        if(ind==arr.size()){
+              if(target==0)
+                ans.push_back(ds);
+            
+                return;
+           
+        
+        }
+        if(arr[ind]<=target){
+         ds.push_back(arr[ind]);
+         printCombination(ind , target-arr[ind] , arr , ds , ans);
+         ds.pop_back();
+        }
+        
+        printCombination(ind+1 , target , arr , ds , ans);
+
+    }
+    
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>>ans;
+       vector<vector<int>>ans;
         vector<int>ds;
-        printCombination(0,candidates,target,ans , ds);
+       
+        
+        printCombination(0 , target  , candidates , ds , ans );
+        
         return ans;
         
         
