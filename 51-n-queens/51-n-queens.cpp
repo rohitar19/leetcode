9ad5d7b2463f9f752a9c5854a1e4a4e4fc38fbcf
rohitar19 <91067722+rohitar19@users.noreply.github.com>
2022-07-29 11:27:19 +0530

    Time: 2 ms (97.11%), Space: 7.3 MB (70.91%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,37 +1,37 @@
 class Solution {
     public:
     
-    bool isSafe( int col , int row , vector<string>& board , int n){
-     int    duprow=row;
-       int  dupcol=col;
+//     bool isSafe( int col , int row , vector<string>& board , int n){
+//      int    duprow=row;
+//        int  dupcol=col;
         
-        while(col>=0 && row>=0){
-            if(board[row][col]=='Q')return false;
-            row--;
-            col--;
-        }
+//         while(col>=0 && row>=0){
+//             if(board[row][col]=='Q')return false;
+//             row--;
+//             col--;
+//         }
         
-       row=duprow;
-        col=dupcol;
-        while(col>=0){
-             if(board[row][col]=='Q')return false;
-            col--;
-        }
+//        row=duprow;
+//         col=dupcol;
+//         while(col>=0){
+//              if(board[row][col]=='Q')return false;
+//             col--;
+//         }
         
-        row=duprow;
-        col=dupcol;
+//         row=duprow;
+//         col=dupcol;
         
-        while(col>=0 && row<n){
-             if(board[row][col]=='Q')return false;
-            col--;
-            row++;
-        }
+//         while(col>=0 && row<n){
+//              if(board[row][col]=='Q')return false;
+//             col--;
+//             row++;
+//         }
         
-        return true;
+//         return true;
         
-    }
+//     }
     
-    void slove( int col , vector<string>&board , vector<vector<string>>&ans , int n ){
+    void slove( int col , vector<string>&board , vector<vector<string>>&ans , int n, vector<int>&siderow , vector<int>&updig , vector<int>&lowdig ){
         
         if(col==n){
             ans.push_back(board);
@@ -39,11 +39,18 @@ class Solution {
         }
         
         for(int row=0 ; row<n ; row++){
-            if( isSafe(col , row , board , n)){
-                
+            // if( isSafe(col , row , board , n))
+            
+            if( siderow[row]==0 && updig[row+col]==0  && lowdig[(n-1) + (col-row)]==0){
                 board[row][col]='Q';
-                slove(col+1 , board , ans , n);
+                siderow[row]=1;
+                updig[row+col]=1;
+                lowdig[n-1+col-row]=1;
+                slove(col+1 , board , ans , n , siderow , updig , lowdig);
                 board[row][col]='.';
+                siderow[row]=0;
+                updig[row+col]=0;
+                lowdig[n-1+col-row]=0;
             }
             
         }
@@ -53,6 +60,9 @@ public:
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>>ans;
         vector<string>board(n);
+        vector<int>siderow(n,0) ;
+        vector<int>updig( 2 * n -1  , 0);
+        vector<int>lowdig(2 * n -1 , 0);
         
          string s(n , '.');
         
@@ -60,7 +70,7 @@ public:
             board[i]=s;
         }
         
-        slove(0, board , ans , n);
+        slove(0, board , ans , n , siderow , updig , lowdig);
         return ans;
     }
 };
