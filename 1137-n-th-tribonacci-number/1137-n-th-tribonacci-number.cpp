084a0031f4ab57d749a5fc7ee0d11584351f41bf
rohitar19 <91067722+rohitar19@users.noreply.github.com>
2022-08-03 11:36:23 +0530

    Time: 0 ms (100.00%), Space: 6 MB (31.04%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -4,28 +4,38 @@ public:
          vector<int>dp(n+1 , -1);
          if(n==0 || n==1) return n;
         
-        dp[0]=0 , dp[1]=1 , dp[2]=1;
+        // dp[0]=0 , dp[1]=1 , dp[2]=1;
+        
+        int prev3=0;
+        int prev2=1;
+        int prev=1;
+        
         
          for(int i=3 ; i<=n ; i++){
               
-             dp[i]=dp[i-3]+ dp[i-2] + dp[i-1];
+             // dp[i]=dp[i-3]+ dp[i-2] + dp[i-1];
+             int curi=prev3+prev2+prev;
+             
+             prev3=prev2;
+             prev2=prev;
+             prev=curi;
              
          }
           
-      return dp[n];
+      return prev;
           
     }
     
-     int  tribo(int n , vector<int>&dp){
+//      int  tribo(int n , vector<int>&dp){
          
-            if(n<=0 ) return 0;
+//             if(n<=0 ) return 0;
          
-          if( n==1 || n==2) return 1;
+//           if( n==1 || n==2) return 1;
          
-          if(dp[n]!=-1) return dp[n];
+//           if(dp[n]!=-1) return dp[n];
          
-           int val= tribo(n-3 , dp)+tribo(n-2 , dp)+tribo(n-1 , dp);
+//            int val= tribo(n-3 , dp)+tribo(n-2 , dp)+tribo(n-1 , dp);
          
-          return dp[n]=val;
-     }
+//           return dp[n]=val;
+//      }
 };
