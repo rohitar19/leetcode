ba1212176b1209bd639db501731dbfecbed06707
rohitar19 <91067722+rohitar19@users.noreply.github.com>
2023-08-25 12:34:12 +0530

    Added solution - LeetHub

--- Floor in BST - GFG/floor-in-bst.cpp
+++ Floor in BST - GFG/floor-in-bst.cpp
@@ -57,20 +57,22 @@ int main() {
 // Function to search a node in BST.
 
 int floor(Node* root, int x) {
-    if(root==NULL) return -1;
-    int floor=-1;
-    while(root){
-        if(root->data==x){
-            floor=root->data;
-            return floor;
-        }
-        if(root->data>x){
-            root=root->left;
-        }
-        else{
+     if(root==NULL) return -1;
+     int floor=-1;
+     while(root){
+         if(root->data==x){
+             floor=root->data;
+             return floor;
+         }
+         
+         if(root->data >x){
+             root=root->left;
+         }
+         else{
             floor=root->data;
             root=root->right;
-        }
-    }
+         }
+     }
+     
     return floor;
 }
