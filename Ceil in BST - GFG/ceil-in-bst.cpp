e1f87116f72a949dff6067a48f59c16620d4933c
rohitar19 <91067722+rohitar19@users.noreply.github.com>
2023-08-27 22:20:56 +0530

    Added solution - LeetHub

--- Ceil in BST - GFG/ceil-in-bst.cpp
+++ Ceil in BST - GFG/ceil-in-bst.cpp
@@ -102,21 +102,20 @@ int main() {
 int findCeil(Node* root, int input) {
     if (root == NULL) return -1;
 
-    int ceil=-1;
-    while(root){
-        if(root->data==input){
-            ceil=root->data;
-            return ceil;
-        }
-        
-        if(root->data>input){
-            ceil=root->data;
-            root=root->left;
-            
-        }
-        else{
-            root=root->right;
-        }
-    }
-    return ceil;
+     int ceil=-1;
+     while(root){
+         if(root->data ==input){
+             ceil=root->data;
+             return ceil;
+         }
+         
+         if(root->data > input){
+             ceil=root->data;
+             root=root->left;
+         }
+         else{
+             root=root->right;
+         }
+     }
+     return ceil;
 }
